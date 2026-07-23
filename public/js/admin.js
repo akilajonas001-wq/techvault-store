469f6a54fba7f082458624a1487146e28abbe495
akilajonas001-wq <akilajonas001@gmail.com>
2026-07-22 21:43:35 -0300

    fix: use fetch directly in toggleProcessado since adminFetch is not available in painel.html

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,5 +3,5 @@ async function adminFetch(url, options = {}) {
   const token = localStorage.getItem('techvault-token');
   const headers = { 'Authorization': 'Bearer ' + token, ...(options.headers || {}) };
   const res = await fetch(url, { ...options, headers });
-  return res;
+  return res.json();
 }
