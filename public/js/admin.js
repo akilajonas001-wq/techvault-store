92de99307ffc6e118ec93a8de328e410ea6caa6d
akilajonas001-wq <akilajonas001@gmail.com>
2026-07-22 21:26:29 -0300

    fix: adminFetch now supports options (method, body, headers) for PUT requests

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,7 @@
 // Admin JS - Funções compartilhadas
-async function adminFetch(url) {
+async function adminFetch(url, options = {}) {
   const token = localStorage.getItem('techvault-token');
-  const res = await fetch(url, {
-    headers: { 'Authorization': 'Bearer ' + token }
-  });
-  return res.json();
+  const headers = { 'Authorization': 'Bearer ' + token, ...(options.headers || {}) };
+  const res = await fetch(url, { ...options, headers });
+  return res;
 }
